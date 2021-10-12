0ec096823f3116328b10ff0c0208843298422f8d
Grégory <gregory.hue135@gmail.com>
2021-10-12 17:36:19 +0200

    update trying to work round manager

--- Assets/VR-Vs-KMS/Scripts/RoundManager.cs
+++ Assets/VR-Vs-KMS/Scripts/RoundManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoundManager : MonoBehaviour
 {
+    public Text score;
 
-    private bool gameOnGoing;
+    private bool gameOnGoing = false;
 
     private List<GameObject> VRPlayer;
 
@@ -17,21 +19,41 @@ public class RoundManager : MonoBehaviour
         {
 
             keyboardPlayer.Add(kPlayer);
+            Debug.Log("Object added");
         }
 
         foreach (GameObject vPlayer in GameObject.FindGameObjectsWithTag("VRPlayer"))
         {
 
             VRPlayer.Add(vPlayer);
+            Debug.Log("Object added");
         }
 
-
+        gameOnGoing = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(VRPlayer.Count);
+
+        if(VRPlayer.Count <= 0)
+        {
+            score.text = "kPlayers Win";
+            gameOnGoing = false;
+
+        } else if(keyboardPlayer.Count <= 0)
+        {
+            score.text = "vPlayers Win";
+            gameOnGoing = false;
+
+        } else
+        {
+            score.text = "Number of kPlayer : " + VRPlayer.Count + " | Number of vPlayer : " + keyboardPlayer.Count;
+            gameOnGoing = true;
+        }
+
+
     }
 }
